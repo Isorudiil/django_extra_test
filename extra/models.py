643e49fd6b8367a25ebe14aa4b1dc54a81cd322e
Elijah Herald <elijah.herald@gmail.com>
2025-04-11 14:19:27 -0400

    cleaned gitignore

--- extra/models.py
+++ extra/models.py
@@ -1,3 +1,23 @@
+"""
+Expected classes:
+    - Workout
+        - wo_type: strfield
+        - wo_duration: intfield
+        - wo_date: datetimefield
+        - wo_intensity: intfield
+        - wo_set_qty: intfield
+        - wo_qty_of_set: intfield
+"""
 from django.db import models
 
-# Create your models here.
+
+class Workout(models.Model):
+    wo_date = models.DateTimeField("workout date", auto_now_add=True)
+    wo_type = models.CharField("workout type", max_length=50)
+    wo_intensity = models.IntegerField("workout intensity")
+    wo_set_qty = models.IntegerField("workout set quantity")
+    wo_qty_of_set = models.IntegerField("workout quantity of set")
+    wo_duration = models.IntegerField("workout duration")
+
+    def __str__(self):
+        return f"{self.wo_type} | {self.wo_duration} | {self.wo_date.strftime('%Y-%m-%d')}min"
